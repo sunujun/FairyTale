4ccfb1a5313de7e5cea24ff04242b517e82c8268
seongjun.oh <tiger8708@gmail.com>
2022-11-13 08:36:02 +0900

    color 관련 코드 추가

---
+++ src/styles/color.ts
@@ -0,0 +1,43 @@
+export default {
+    /** 글자 색 */
+    text: {
+        /** ![](https://www.colorhexa.com/333333.png) */
+        primary1: '#333333',
+        /** ![](https://www.colorhexa.com/000000.png) */
+        primary2: '#000000',
+        /** ![](https://www.colorhexa.com/666666.png) */
+        secondary1: '#666666',
+        /** ![](https://www.colorhexa.com/999999.png) */
+        secondary2: '#999999',
+        /** ![](https://www.colorhexa.com/bbbbbb.png) */
+        hint: '#bbbbbb',
+        /** ![](https://www.colorhexa.com/a26077.png) */
+        main: '#98556c',
+        /** ![](https://www.colorhexa.com/ff5e73.png) */
+        caution1: '#ff5e73',
+        /** ![](https://www.colorhexa.com/166bff.png) */
+        caution2: '#166bff',
+    },
+    /** ![](https://www.colorhexa.com/eeeeee.png) */
+    divider: '#eeeeee',
+    /** 버튼 색상 */
+    button: {
+        /** ![](https://www.colorhexa.com/98556c.png) */
+        primary: '#98556c',
+        /** ![](https://www.colorhexa.com/fff3f1.png) */
+        secondary: '#fff3f1',
+        /** ![](https://www.colorhexa.com/ffffff.png) */
+        disabled: '#ffffff',
+        /** ![](https://www.colorhexa.com/ccaab6.png) */
+        disabledPrimary: '#ccaab6',
+        /** ![](https://www.colorhexa.com/eeeeee.png) */
+        border: '#eeeeee',
+    },
+    /** 배경 색상 */
+    background: {
+        /** ![](https://www.colorhexa.com/ffffff.png) */
+        primary: '#ffffff',
+        /** ![](https://www.colorhexa.com/f5f5f5.png) */
+        secondary: '#f5f5f5',
+    },
+};
